89a88943d3e2df54baad06f2a417d3bf867f0292
Gabriel Nascimento <ytshopper16@gmail.com>
2026-02-20 22:14:45 -0400

    Creating a C program to know if numbers are equal!

---
+++ C/NumIguais/NumIguais.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+     setlocale(LC_ALL, "Portuguese");
+
+    int n1 , n2; 
+
+    printf("Digite aqui o primeiro numero: ");
+    scanf("%d" , &n1);
+
+    printf("Digite aqui o segundo numero: ");
+    scanf("%d" , &n2);
+
+    if( n1 > n2 ){
+        printf("O maior numero é: %d" , n1);
+    }else{
+
+        if( n2 > n1 ){
+            printf("O maior numero é: %d" , n2);
+        }else{
+            printf("Os numeros são iguais!!");
+        }
+        
+    }
+
+    return 0;
+
+}
